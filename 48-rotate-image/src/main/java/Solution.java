48cc3d0edb102533adf1696cda2a9e60592f0c01
Dzmitry Rafalovich <drafalovich@evolution.com>
2023-09-19 22:45:49 +0300

    Added solution for task 48 (Rotate Matrix)

---
+++ 48-rotate-image/src/main/java/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int length = matrix.length;
+
+        // Matrix transpose
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Swap the elements of the first column with the last column.
+        // The second column with the last one - 1 column and so on.
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][length - 1 - j];
+                matrix[i][length - 1 - j] = temp;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -38,4 +38,5 @@
 | 71  |             Simplify Path              |            Medium |                         String, Stack |
 | 150 |    Evaluate Reverse Polish Notation    |            Medium |                          Array, Stack |
 | 36  |              Valid Sudoku              |            Medium |             Array, Hash Table, Matrix |
+| 48  |              Rotate Image              |            Medium |                   Array, Math, Matrix |
 
